41c8ed96d084b22df551cd7510be3106209cfac8
agaramv <42156415+agaramv@users.noreply.github.com>
2021-05-20 10:53:42 -0400

    Created New spring boot project

---
+++ acems/src/main/java/com/ace/acems/AcemsApplication.java
@@ -0,0 +1,13 @@
+package com.ace.acems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcemsApplication.class, args);
+	}
+
+}
---
+++ acems/src/test/java/com/ace/acems/AcemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.acems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
