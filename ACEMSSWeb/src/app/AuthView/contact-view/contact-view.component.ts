7b96a6385f768d974ce8c5588b8f5f0e8c09fa79
agaramv <42156415+agaramv@users.noreply.github.com>
2021-05-24 22:02:35 -0400

    Created contact message view

---
+++ ACEMSSWeb/src/app/AuthView/contact-view/contact-view.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/app.service';
+
+@Component({
+  selector: 'app-contact-view',
+  templateUrl: './contact-view.component.html',
+  styleUrls: ['./contact-view.component.css']
+})
+export class ContactViewComponent implements OnInit {
+  messages = []
+  columns = ["First Name", "Last Name","Email", "Phone", "Message"]
+  constructor(private appService: AppService) { }
+
+  ngOnInit(): void {
+    this.getAllMessages()
+  }
+
+  getAllMessages(){
+    this.appService.getAllMessages().subscribe(data=>{
+      console.log(data);
+      this.messages = data;
+    })
+  }
+}
